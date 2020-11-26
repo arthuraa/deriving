d882b6391f31e19a123d69e42b6d6f1e009db1ea
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-11-26 16:24:01 -0500

    Inference for mutual inductives seems to be working!

--- theories/examples.v
+++ theories/examples.v
@@ -72,6 +72,52 @@ Canonical three_orderType :=
 
 End FiniteExample.
 
+Section MutualExample.
+
+Unset Elimination Schemes.
+Inductive foo :=
+| Foo1 of nat
+| Foo2 of bool & bar
+
+with bar :=
+| Bar1 of foo & foo
+| Bar2 of nat & foo.
+Set Elimination Schemes.
+
+Scheme foo_rect := Induction for foo Sort Type
+with   bar_rect := Induction for bar Sort Type.
+
+Combined Scheme foo_bar_rect from foo_rect, bar_rect.
+
+Definition foo_bar_mutIndMixin := [mutIndType for foo_bar_rect].
+Canonical foo_indType := IndType _ foo foo_bar_mutIndMixin.
+Canonical bar_indType := IndType _ bar foo_bar_mutIndMixin.
+
+Definition foo_eqMixin := [derive eqMixin for foo].
+Canonical foo_eqType := EqType foo foo_eqMixin.
+Definition bar_eqMixin := [derive eqMixin for bar].
+Canonical bar_eqType := EqType bar bar_eqMixin.
+Definition foo_choiceMixin := [derive choiceMixin for foo].
+Canonical foo_choiceType := Eval hnf in ChoiceType foo foo_choiceMixin.
+Definition bar_choiceMixin := [derive choiceMixin for bar].
+Canonical bar_choiceType := Eval hnf in ChoiceType bar bar_choiceMixin.
+Definition foo_countMixin := [derive countMixin for foo].
+Canonical foo_countType := Eval hnf in CountType foo foo_countMixin.
+Definition bar_countMixin := [derive countMixin for bar].
+Canonical bar_countType := Eval hnf in CountType bar bar_countMixin.
+Definition foo_orderMixin := [derive orderMixin for foo].
+Canonical foo_porderType := Eval hnf in POrderType tt foo foo_orderMixin.
+Canonical foo_latticeType := Eval hnf in LatticeType foo foo_orderMixin.
+Canonical foo_distrLatticeType := Eval hnf in DistrLatticeType foo foo_orderMixin.
+Canonical foo_orderType := Eval hnf in OrderType foo foo_orderMixin.
+Definition bar_orderMixin := [derive orderMixin for bar].
+Canonical bar_porderType := Eval hnf in POrderType tt bar bar_orderMixin.
+Canonical bar_latticeType := Eval hnf in LatticeType bar bar_orderMixin.
+Canonical bar_distrLatticeType := Eval hnf in DistrLatticeType bar bar_orderMixin.
+Canonical bar_orderType := Eval hnf in OrderType bar bar_orderMixin.
+
+End MutualExample.
+
 Module SyntaxExample.
 
 (* An example of syntax trees for a lambda calculus.  Adapted from Iris's heap
