9ed8e1e17b6ee0b0485e48d1b7f8eb6078194f2d
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-11-30 15:03:05 -0500

    Move test files to different folder.

---
+++ tests/nested.v
@@ -0,0 +1,51 @@
+From mathcomp Require Import
+  ssreflect ssrfun ssrbool ssrnat eqtype seq choice fintype finset order.
+
+From deriving Require Import deriving.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Unset Elimination Schemes.
+Inductive rose := Rose of seq rose.
+Set Elimination Schemes.
+
+Definition rose_rect
+  (P1 : rose -> Type)
+  (P2 : seq rose -> Type)
+  (HR : forall rs, P2 rs -> P1 (Rose rs))
+  (HN : P2 [::])
+  (HC : forall r, P1 r -> forall rs, P2 rs -> P2 (r :: rs))
+  : forall r, P1 r :=
+  fix rose_rect r :=
+    let fix seq_rose_rect rs : P2 rs :=
+        match rs with
+        | [::] => HN
+        | r :: rs => HC r (rose_rect r) rs (seq_rose_rect rs)
+        end in
+    match r with Rose rs => HR rs (seq_rose_rect rs) end.
+
+Definition seq_rose_rect
+  (P1 : rose -> Type)
+  (P2 : seq rose -> Type)
+  (HR : forall rs, P2 rs -> P1 (Rose rs))
+  (HN : P2 [::])
+  (HC : forall r, P1 r -> forall rs, P2 rs -> P2 (r :: rs))
+  : forall rs, P2 rs :=
+    fix seq_rose_rect rs : P2 rs :=
+      match rs with
+      | [::] => HN
+      | r :: rs => HC r (rose_rect HR HN HC r) rs (seq_rose_rect rs)
+      end.
+
+Combined Scheme rose_seq_rose_rect from rose_rect, seq_rose_rect.
+
+Definition rose_seq_rose_indMixin := [indMixin for rose_seq_rose_rect].
+Canonical rose_indType := IndType _ rose rose_seq_rose_indMixin.
+Definition rose_eqMixin := [derive eqMixin for rose].
+Canonical rose_eqType := EqType rose rose_eqMixin.
+Definition rose_choiceMixin := [derive choiceMixin for rose].
+Canonical rose_choiceType := Eval hnf in ChoiceType rose rose_choiceMixin.
+Definition rose_countMixin := [derive countMixin for rose].
+Canonical rose_countType := Eval hnf in CountType rose rose_countMixin.
---
+++ tests/tree.v
@@ -0,0 +1,31 @@
+From mathcomp Require Import
+  ssreflect ssrfun ssrbool ssrnat eqtype seq choice fintype finset order.
+
+From deriving Require Import deriving.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Inductive tree (T : Type) :=
+| Leaf of {set 'I_10}
+| Node of T & tree T & tree T.
+Arguments Leaf {_} _.
+
+Definition tree_indMixin T :=
+  [indMixin for @tree_rect T].
+Canonical tree_indType T :=
+  Eval hnf in IndType _ (tree T) (tree_indMixin T).
+
+Definition tree_eqMixin (T : eqType) :=
+  [derive eqMixin for tree T].
+Canonical tree_eqType (T : eqType) :=
+  Eval hnf in EqType (tree T) (tree_eqMixin T).
+Definition tree_choiceMixin (T : choiceType) :=
+  [derive choiceMixin for tree T].
+Canonical tree_choiceType (T : choiceType) :=
+  Eval hnf in ChoiceType (tree T) (tree_choiceMixin T).
+Definition tree_countMixin (T : countType) :=
+  [derive countMixin for tree T].
+Canonical tree_countType (T : countType) :=
+  Eval hnf in CountType (tree T) (tree_countMixin T).
