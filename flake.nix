6433fdc3a2eb07ed938f6414a2501cb650289dc6
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2026-01-03 18:19:05 -0300

    Use GitHub Actions instead of CircleCI.

--- flake.nix
+++ flake.nix
@@ -6,28 +6,66 @@
     define their own instances for other classes.
   '';
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-github-actions.url = "github:nix-community/nix-github-actions";
+    nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in rec {
-            packages = rec {
-              coq = pkgs.coq_8_19;
-              coqPackages = pkgs.coqPackages_8_19.overrideScope (self: super:
-                { mathcomp = super.mathcomp.override {
-                    version = "2.2.0";
-                  };
-                  deriving = super.deriving.overrideAttrs {
-                    version = "0.2.1";
-                    src = ./.;
-                  };
-                });
-              ocaml = pkgs.ocaml;
-            };
+  outputs = inputs@{ self, flake-parts, nix-github-actions, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
 
-            devShell = pkgs.mkShell {
-              packages = with packages; [ coq coqPackages.mathcomp.ssreflect ocaml ];
-            };
-      }
-    );
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        _module.args.pkgs = import self.inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+          ];
+          config = { };
+        };
+
+        devShells.default = pkgs.mkShell {
+          propagatedBuildInputs = [
+            pkgs.coqPackages.coq-lsp
+          ];
+          inputsFrom = [
+            self'.packages.default
+          ];
+        };
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages.default = pkgs.coqPackages.deriving;
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+        githubActions = nix-github-actions.lib.mkGithubMatrix {
+          checks = self.packages;
+        };
+
+        overlays.default = final: prev: {
+          coqPackages = prev.coqPackages.overrideScope (final: prev: {
+            deriving = prev.lib.overrideCoqDerivation {
+              defaultVersion = "dev";
+              release.dev.src = ./.;
+            } prev.deriving;
+          });
+        };
+
+      };
+    };
 }
